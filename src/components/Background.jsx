9547657baf5da0990c0a1d45e004822e94c93b1a
MiloszWrobel <wrobm006.209@gmail.com>
2025-05-14 15:07:54 +0100

    Changed background image to be darker

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -15,18 +15,16 @@ export default function Background() {
       <img
         src={cityImage}
         alt="background"
-        className="w-full object-cover saturate-80"
+        className="w-full  object-cover saturate-80 brightness-50"
         style={{ height: "100vh" }}
       />
-      <h2
-        className=" absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold text-white bg-slate-800 rounded-2xl bg-opacity-70 p-2 border-slate-800 border-2 "
-        style={{ top: "50vh" }}>
+      <h2 className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold text-white">
         Milosz Wrobel
       </h2>
 
       <div
         className="absolute left-1/2 transform -translate-x-1/2 flex gap-6"
-        style={{ top: "75vh" }}>
+        style={{ top: "55vh" }}>
         <a
           href="https://www.linkedin.com/in/milosz-w/"
           className="p-4 bg-slate-800 text-white rounded-full hover:bg-slate-900">
@@ -36,14 +34,14 @@ export default function Background() {
         <a
           href="https://github.com/MiloszWrobel"
           className="p-4 bg-slate-800 text-white rounded-full hover:bg-slate-900">
-          <SiGithub className="w-6 h-6 text-white" /> {/* GitHub Icon */}
+          <SiGithub className="w-6 h-6 text-white" />
         </a>
       </div>
 
       <button
         onClick={handleScrollClick}
-        className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 bg-slate-800 text-white rounded-full hover:bg-slate-900"
-        style={{ top: "85vh" }}>
+        className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 bg-slate-800 text-white rounded-full hover:bg-slate-900 cursor-pointer"
+        style={{ top: "68vh" }}>
         <ChevronDownIcon className="w-6 h-4 text-white" />
       </button>
     </div>
